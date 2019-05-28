14fd532a26cc7d43db2024fe28f8fa9d26bbf19a
Julyana Bello <julyana66@hotmail.com>
2019-05-28 18:23:05 -0300

    Update Eletronicos.java

--- Eletronicos.java
+++ Eletronicos.java
@@ -1,163 +1,124 @@
 package av2;
 
-public class Eletronicos {
-	private String tipo;
+
+public class Eletronicos extends Produtos{
+
 	private String cor;
-	private String fabricante;
 	private double largura;
 	private double altura;
 	private double cod;
-	private double preço;
 	private int volume;
+	
+	
 
-	public Eletronicos()
-	{
+	public Eletronicos(){
 
 	}
 
-	public Eletronicos(String tipo,String cor, String fabricante, double largura,double altura, double cod, double preço)
-	{
-		this.tipo=tipo;
+	public Eletronicos(String cor, double largura,double altura, double cod, int volume){
+
+
 		this.cor=cor;
-		this.fabricante=fabricante;
 		this.altura=altura;
 		this.largura=largura;
 		this.cod=cod;
-		this.preço=preço;
+		this.volume=volume;
+
 	}
-
-
-
-	public String getTipo()
-	{
-		return tipo;
-	}
-
-	public void setTipo(String tipo)
-	{
-		this.tipo = tipo;
-	}
-
-	public String getCor()
-	{
+	
+	public String getCor(){
 		return cor;
+
 	}
 
-	public void setCor(String cor)
-	{
+	public void setCor(String cor){
 		this.cor = cor;
+
 	}
 
-	public String getFabricante()
-	{
-		return fabricante;
-	}
 
-	public void setFabricante(String fabricante)
-	{
-		this.fabricante = fabricante;
-	}
-
-	public double getAltura()
-	{
+	public double getAltura(){
 		return altura;
+
 	}
 
-	public void setAltura(double altura)
-	{
+	public void setAltura(double altura){
 		this.altura = altura;
+
 	}
 
-	public double getLargura()
-	{
+	public double getLargura(){
 		return largura;
+
 	}
 
-	public void setLargura(double largura)
-	{
+	public void setLargura(double largura){
 		this.largura = largura;
+
 	}
 
-	public double getCod()
-	{
+	public double getCod(){
 		return cod;
+
 	}
 
-	public void setCod(double cod)
-	{
+	public void setCod(double cod) {
 		this.cod = cod;
+
 	}
 
-	public double getPreço()
-	{
-		return preço;
-	}
 
-	public void setPreço(double preço)
-	{
-		this.preço = preço;
-	}
-
-	public int getVolume()
-	{
+	public int getVolume(){
 		return volume;
+
 	}
 
-	public void setVolume(int volume)
-	{
+	public void setVolume(int volume) {
 		this.volume = volume;
+
 	}
 
-	public void ligar()
-	{
+	public void ligar() {
 		System.out.println("ligado");
+
 	}
-	public void desligar()
-	{
+
+	public void desligar(){
 		System.out.println("desligado");
+
 	}
-	public int aumentarsom(int getvolume)
-	{
+
+	public int aumentarsom(int getvolume){
 		return getvolume;
-	}
-	public void ligação()
-	{
-
 
 	}
-	public void mudarcanal()
-	{
 
-	}
-	public void mudarestação()
-	{
 
-	}
-	public int mostrarcodigo(int getcod)
-	{
+	public int mostrarcodigo(int getcod){
 		return getcod;
+
 	}
 
-	public String mostrarcor(String cor)
-	{
+	public String mostrarcor(String cor){
 		return getCor();
+
 	}
-	public double mostrarlargura(double largura)
-	{
+
+	public double mostrarlargura(double largura){
 		return getLargura();
+
 	}
-	public double mostraraltura(double altura)
-	{
+
+	public double mostraraltura(double altura){
 		return getAltura();
+
 	}
-	public double mostrarpreço(double preço)
-	{
-		return getPreço();
-	}
-	public void comprarproduto()
-	{
-		System.out.println("O produto está no carrinho");
-	}
+
 
 
 }
+
+
+
+
+
