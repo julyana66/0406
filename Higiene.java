bce4257cf59a681980300637480930dfd09fe5a9
Julyana Bello <julyana66@hotmail.com>
2019-05-28 18:24:04 -0300

    Update Higiene.java

--- Higiene.java
+++ Higiene.java
@@ -1,34 +1,25 @@
 package av2;
 
-public class Higiene {
-	private String tipo;
+
+public class Higiene extends Produtos {
+
 	private String nome;
 	private float pesoliquido;
 	private double coddebarras;
-	private String fabricante;
-	private double preço;
 
 	public Higiene(){
 
 	}
 
-	public Higiene(String tipo,String nome,float pesoliquido,double coddebarras,String fabricante,double preço)
-	{
-		this.tipo=tipo;
+	public Higiene(String nome,float pesoliquido,double coddebarras){
+
 		this.nome=nome;
 		this.pesoliquido=pesoliquido;
 		this.coddebarras=coddebarras;
-		this.fabricante=fabricante;
-		this.preço=preço;
+
+
 	}
 
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
 
 	public String getNome() {
 		return nome;
@@ -54,39 +45,17 @@ public class Higiene {
 		this.coddebarras = coddebarras;
 	}
 
-	public String getFabricante() {
-		return fabricante;
-	}
 
-	public void setFabricante(String fabricante) {
-		this.fabricante = fabricante;
-	}
-
-	public double getPreço() {
-		return preço;
-	}
-
-	public void setPreço(double preço) {
-		this.preço = preço;
-	}
-
-
-	public float mostrarML(float pesoliquido)
-	{
+	public float mostrarPesoLiq (float pesoliquido){
 		return getPesoliquido();
-	}
-	public double mostrarcod(double coddebarras)
-	{
-		return getCoddebarras();
+
 	}
 
-	public double mostrarpreço (double preço)
-	{
-		return getPreço();
+	public double mostrarcod(double coddebarras){
+		return getCoddebarras();
+
 	}
-	public void comprarproduto()
-	{
-		System.out.println("O produto está no carrinho");
-	}
+
+
 
 }
