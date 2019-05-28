305e905c8dfe17fdbf437124d267469719c3d5c3
Julyana Bello <julyana66@hotmail.com>
2019-05-28 18:18:50 -0300

    Update Alimentos.java

--- Alimentos.java
+++ Alimentos.java
@@ -1,70 +1,39 @@
 package av2;
 
-public class Alimentos {
-	private String tipo;
-	private String fabricante;
+
+public class Alimentos extends Produtos {
+
+
 	private String peso;
 	private double codigodebarras;
-	private double preço;
 
-	public Alimentos()
-	{
 
+	public String getPeso() { 
+		return peso; 
 	}
 
-	public Alimentos(String tipo,String fabricante,String peso,double codigodebarras,double preço)
-	{
-		this.tipo=tipo;
-		this.fabricante=fabricante;
-		this.peso=peso;
-		this.codigodebarras=codigodebarras;
-		this.preço=preço;
+	public void setPeso(String peso) { 
+		this.peso = peso; 
 	}
 
-	public String getTipo() { return tipo; }
-
-	public void setTipo(String tipo) { this.tipo = tipo; }
-
-	public String getFabricante() { return fabricante; }
-
-	public void setFabricante(String fabricante) { this.fabricante = fabricante; }
-
-	public String getPeso() { return peso; }
-
-	public void setPeso(String peso) { this.peso = peso; }
-
-	public double getCodigodebarras() { return codigodebarras;}
-
-	public void setCodigodebarras(double codigodebarras) { this.codigodebarras = codigodebarras; }
-
-	public double getPreço() { return preço; }
-
-	public void setPreço(double preço) { this.preço = preço; }
-
-
-	public String mostrarpeso (String peso)
-	{
-		return getPeso();
-	}
-
-	public String mostrartipo (String tipo)
-	{
-		return getTipo();
-	}
-
-	public double mostrarcodigo(double codigodebarras)
-	{
+	public double getCodigodebarras() { 
 		return codigodebarras;
 	}
 
-	public double mostrarpreço (double preço)
-	{
-		return getPreço();
+	public void setCodigodebarras(double codigodebarras) { 
+		this.codigodebarras = codigodebarras; 
 	}
 
-	public void comprarproduto()
-	{
-		System.out.println("O produto está no carrinho");
+
+	public String mostrarpeso (String peso)	{
+		return getPeso();
 	}
+
+
+	public double mostrarcodigo(double codigodebarras){
+		return codigodebarras;
+	}
+
+
 
 }
